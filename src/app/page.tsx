049fbbe1d1a775afa7dbef7f05c88840a6c893b4
LY Heang <lyheang4248@gmail.com>
2024-06-25 13:33:45 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="font-black text-gray-900 text-[32px] w-full h-screen flex items-center justify-center">
+        TO-DO LIST APPLICATIONS
+      </h1>
+    </div>
+  );
+};
+
+export default page;
